c4ac10f1d48ed97994d918753760172266de121a
sofia-araujo <sofiamarcelino2804@gmail.com>
2025-10-27 12:41:58 -0300

    :memo: Criada a documentação do Swagger

--- src/main/java/com/generation/farmacia/model/Usuario.java
+++ src/main/java/com/generation/farmacia/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.generation.farmacia.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class Usuario {
 	@NotBlank(message = "O Atributo Nome é Obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotBlank(message = "O Atributo Usuário é Obrigatório!")
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
